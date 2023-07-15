63ca72a6837ab71c958926eb715c28d00bbd2aac
Andrew <andrewwasfy24@gmail.com>
2023-07-15 17:15:25 +0300

    create search view and add it to router

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
+import 'package:bookly/features/search/presentaion/views/search_view.dart';
 import 'package:bookly/features/splash/presentaion/views/splash.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = "/homeView";
   static const kBookDetailsView = "/bookDetailsView";
+  static const kSearchView = "/searchView";
 
  static final  router = GoRouter(
     routes: <RouteBase>[
@@ -25,6 +27,12 @@ abstract class AppRouter {
         path: kBookDetailsView,
         builder: (context, GoRouterState state) {
           return const BookDetailsView();
+        },
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, GoRouterState state) {
+          return const SearchView();
         },
       ),
     ],
