147887344c540e52567c7a101cea5d731e0875a5
Andrew <andrewwasfy24@gmail.com>
2023-07-17 18:20:39 +0300

    create failure class

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model/book_model.dart';
+import 'package:either_dart/either.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
