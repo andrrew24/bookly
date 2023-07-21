a646b45db17883dfb5ed684bd959c118475a91e0
Andrew <andrewwasfy24@gmail.com>
2023-07-21 15:22:13 +0300

    handlinf all exceptions

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,6 @@ import 'package:flutter/material.dart';
 const kprimaryColor = Color(0xff150426);
 
 const kfontFamily = "GTsectra";
+
+const kNewestEndPoint =
+    'https://www.googleapis.com/books/v1/volumes?Filtering=free-ebooks&Sorting=newest &q=subject:computer science';
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,19 +1,34 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/data/models/book_model/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/api_service.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dio/dio.dart';
 import 'package:either_dart/src/either.dart';
 
 class HomeRepoImp extends HomeRepo {
+  late ApiService apiService;
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiService.get(endpoint: kNewestEndPoint);
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(item);
+      }
+      return Right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioException(e));
+      } else {
+        return Left(ServerFailure(errorName: e.toString()));
+      }
+    }
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-   
     throw UnimplementedError();
   }
-  
 }
