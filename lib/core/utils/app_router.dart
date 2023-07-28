bbb5bcc83acccf32bfe042a46559dcfe7eadb941
Andrew <andrewwasfy24@gmail.com>
2023-07-29 02:53:08 +0300

    add blocProvider to bookDetailsView in routes

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,7 +1,11 @@
+import 'package:bookly/core/utils/service_locator.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/features/home/presentation/view_model/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:bookly/features/search/presentaion/views/search_view.dart';
 import 'package:bookly/features/splash/presentaion/views/splash.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
@@ -26,7 +30,10 @@ abstract class AppRouter {
       GoRoute(
         path: kBookDetailsView,
         builder: (context, GoRouterState state) {
-          return const BookDetailsView();
+          return  BlocProvider(
+            create: (context) => SimilarBooksCubit(getIt.get<HomeRepoImp>()),
+            child: const BookDetailsView(),
+          );
         },
       ),
       GoRoute(
