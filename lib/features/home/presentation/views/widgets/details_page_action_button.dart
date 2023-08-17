488a37130510e4282cc46a69ae51c88c2c955ac7
Andrew <andrewwasfy24@gmail.com>
2023-08-17 14:18:06 +0300

    change preview link

--- lib/features/home/presentation/views/widgets/details_page_action_button.dart
+++ lib/features/home/presentation/views/widgets/details_page_action_button.dart
@@ -1,7 +1,10 @@
+import 'package:bookly/core/utils/functions/launch_url.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/core/utils/widgets/custom_button.dart';
 import 'package:bookly/core/data/book_model/book_model.dart';
+import 'package:bookly/features/cart/presentaion/manager/cart_cubit/cart_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../methods/get_text.dart';
 
 class DetailsPageActionButton extends StatelessWidget {
@@ -18,29 +21,35 @@ class DetailsPageActionButton extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         CustomButton(
-          onPressed: () {},
+          onPressed: () {
+            if (!(BlocProvider.of<CartCubit>(context)
+                .books
+                .contains(bookModel))) {
+              BlocProvider.of<CartCubit>(context).addBook(bookModel);
+            }
+          },
           text: "Add To Cart",
           color: Colors.white,
           shape: const MaterialStatePropertyAll(
             RoundedRectangleBorder(
               borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(16),
-                bottomLeft: Radius.circular(16)
-              ),
+                  topLeft: Radius.circular(16),
+                  bottomLeft: Radius.circular(16)),
             ),
           ),
         ),
         CustomButton(
-          onPressed: () {},
+          onPressed: () {
+            launchTheUrl(context, bookModel.accessInfo!.webReaderLink);
+          },
           text: getText(bookModel),
           color: Colors.red,
           textStyle: Styles.textStyle18.copyWith(color: Colors.white),
           shape: const MaterialStatePropertyAll(
             RoundedRectangleBorder(
               borderRadius: BorderRadius.only(
-                topRight: Radius.circular(16),
-                bottomRight: Radius.circular(16)
-              ),
+                  topRight: Radius.circular(16),
+                  bottomRight: Radius.circular(16)),
             ),
           ),
         ),
